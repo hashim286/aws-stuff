b68ef2647f63d84bff14ca32f4c465fc3516d3f1
hashim286 <112742556+hashim286@users.noreply.github.com>
2025-04-10 09:40:10 -0400

    added a backend config in terraform block to keep tfstate file in an S3 bucket for centralized management

--- terraform/seefood-db/provider.tf
+++ terraform/seefood-db/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  # added backend S3 to keep centralized state file
+  backend "s3" {
+    bucket                   = "terraform-state-bucket-hr"
+    key                      = "terraform.tfstate"
+    region                   = "us-east-1"
+    shared_credentials_files = ["~/.aws/credentials"]
+    profile                  = "iamadmin-general"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
